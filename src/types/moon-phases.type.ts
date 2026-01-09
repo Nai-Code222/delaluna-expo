30b1080f7897e997f4f0077bae93707e2beacb3e
Nai-Code222 <jeanairoberts@outlook.com>
2026-01-09 13:31:20 -0600

    up date hroscope prompt to match new moon phase details data type

---
+++ src/types/moon-phases.type.ts
@@ -0,0 +1,8 @@
+// src/types/moon-phases.type.ts
+
+export interface MoonPhaseDetails {
+    moonPhaseName: string;
+    moonPhase: string;
+    moonPhaseDegrees: string;
+    moonPhaseInterpretation: string;
+}
